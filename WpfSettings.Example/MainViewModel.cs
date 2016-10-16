d0b0effd0ed868e8620430a1aa5b53db031f89fa
lelimacon <lelimacon@users.noreply.github.com>
2016-10-16 19:07:20 +0200

    Example settings window shown as dialog

--- WpfSettings.Example/MainViewModel.cs
+++ WpfSettings.Example/MainViewModel.cs
@@ -8,19 +8,16 @@ namespace WpfSettings.Example
     [ImplementPropertyChanged]
     public class MainViewModel
     {
-        #region Properties
-
-        public ICommand ShowSettingsCommand => new RelayCommand(ShowSettings);
+        public ICommand ShowSettingsCommand { get; set; }
         public QuickSettings QuickSettings { get; set; }
         public GlobalSettings Settings { get; set; }
         public string WindowTitle { get; set; }
-
-        #endregion Properties
 
         public MainViewModel()
         {
             Settings = new GlobalSettings();
             QuickSettings = new QuickSettings(Settings);
+            ShowSettingsCommand = new RelayCommand<MainWindow>(ShowSettings);
             SetTitle();
             dynamic userSettings = Settings.User;
             userSettings.PropertyChanged += new PropertyChangedEventHandler(SettingChanged);
@@ -37,11 +34,14 @@ namespace WpfSettings.Example
             WindowTitle = $"{Settings?.User?.Name}'s note";
         }
 
-        private void ShowSettings()
+        private void ShowSettings(MainWindow mainWindow)
         {
-            SettingsWindow window = new SettingsWindow(Settings);
-            window.IconSource = "Resources.icon-cog.png";
-            window.ShowDialog();
+            SettingsWindow settingsWindow = new SettingsWindow(Settings)
+            {
+                IconSource = "Resources.icon-cog.png",
+                Owner = mainWindow
+            };
+            settingsWindow.ShowDialog();
         }
     }
 }
