150ead93f2e3760c27bae96fad6a0419b9b90d0d
lelimacon <lelimacon@users.noreply.github.com>
2016-10-16 19:41:37 +0200

    Config elements full on NotifyPropertyChanged

--- WpfSettings/Config/ConfigElement.cs
+++ WpfSettings/Config/ConfigElement.cs
@@ -1,20 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using WpfSettings.Annotations;
 using WpfSettings.Utils;
 
 namespace WpfSettings.Config
 {
-    public abstract class ConfigElement
+    public abstract class ConfigElement : INotifyPropertyChanged
     {
+        private int _position;
+        private string _label;
         public object Parent { get; }
         public MemberInfo Member { get; }
-        public int Position { get; set; }
-        public string Label { get; set; }
+
+        public int Position
+        {
+            get { return _position; }
+            set
+            {
+                if (value == _position) return;
+                _position = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Label
+        {
+            get { return _label; }
+            set
+            {
+                if (value == _label) return;
+                _label = value;
+                OnPropertyChanged();
+            }
+        }
 
         protected ConfigElement(object parent, MemberInfo member)
         {
@@ -26,13 +51,54 @@ namespace WpfSettings.Config
         public virtual void Save()
         {
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
     public class ConfigSection : ConfigElement
     {
-        public BitmapSource Image { get; set; }
-        public ObservableCollection<ConfigSection> SubSections { get; set; }
-        public ObservableCollection<ConfigPageElement> Elements { get; set; }
+        private BitmapSource _image;
+        private ObservableCollection<ConfigSection> _subSections;
+        private ObservableCollection<ConfigPageElement> _elements;
+
+        public BitmapSource Image
+        {
+            get { return _image; }
+            set
+            {
+                if (Equals(value, _image)) return;
+                _image = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<ConfigSection> SubSections
+        {
+            get { return _subSections; }
+            set
+            {
+                if (Equals(value, _subSections)) return;
+                _subSections = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<ConfigPageElement> Elements
+        {
+            get { return _elements; }
+            set
+            {
+                if (Equals(value, _elements)) return;
+                _elements = value;
+                OnPropertyChanged();
+            }
+        }
 
         public ConfigSection(object parent, MemberInfo member)
             : base(parent, member)
@@ -58,7 +124,18 @@ namespace WpfSettings.Config
 
     public class ConfigGroup : ConfigPageElement
     {
-        public IEnumerable<ConfigPageElement> Elements { get; set; }
+        private IEnumerable<ConfigPageElement> _elements;
+
+        public IEnumerable<ConfigPageElement> Elements
+        {
+            get { return _elements; }
+            set
+            {
+                if (Equals(value, _elements)) return;
+                _elements = value;
+                OnPropertyChanged();
+            }
+        }
 
         public ConfigGroup(object parent, MemberInfo member, IEnumerable<ConfigPageElement> elements)
             : base(parent, member)
@@ -75,8 +152,30 @@ namespace WpfSettings.Config
 
     public class StringConfig : ConfigPageElement
     {
-        public string Value { get; set; }
-        public string Details { get; set; }
+        private string _value;
+        private string _details;
+
+        public string Value
+        {
+            get { return _value; }
+            set
+            {
+                if (value == _value) return;
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Details
+        {
+            get { return _details; }
+            set
+            {
+                if (value == _details) return;
+                _details = value;
+                OnPropertyChanged();
+            }
+        }
 
         public StringConfig(object parent, MemberInfo member)
             : base(parent, member)
@@ -91,9 +190,42 @@ namespace WpfSettings.Config
 
     public class TextConfig : ConfigPageElement
     {
-        public string Value { get; set; }
-        public string Details { get; set; }
-        public int Height { get; set; }
+        private string _value;
+        private string _details;
+        private int _height;
+
+        public string Value
+        {
+            get { return _value; }
+            set
+            {
+                if (value == _value) return;
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Details
+        {
+            get { return _details; }
+            set
+            {
+                if (value == _details) return;
+                _details = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Height
+        {
+            get { return _height; }
+            set
+            {
+                if (value == _height) return;
+                _height = value;
+                OnPropertyChanged();
+            }
+        }
 
         public TextConfig(object parent, MemberInfo member)
             : base(parent, member)
@@ -109,8 +241,30 @@ namespace WpfSettings.Config
 
     public class BoolConfig : ConfigPageElement
     {
-        public bool Value { get; set; }
-        public string Details { get; set; }
+        private bool _value;
+        private string _details;
+
+        public bool Value
+        {
+            get { return _value; }
+            set
+            {
+                if (value == _value) return;
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Details
+        {
+            get { return _details; }
+            set
+            {
+                if (value == _details) return;
+                _details = value;
+                OnPropertyChanged();
+            }
+        }
 
         public BoolConfig(object parent, MemberInfo member)
             : base(parent, member)
@@ -125,9 +279,42 @@ namespace WpfSettings.Config
 
     public class ChoiceConfig : ConfigPageElement
     {
-        public ObservableCollection<string> Choices { get; set; }
-        public string SelectedValue { get; set; }
-        public string Details { get; set; }
+        private ObservableCollection<string> _choices;
+        private string _selectedValue;
+        private string _details;
+
+        public ObservableCollection<string> Choices
+        {
+            get { return _choices; }
+            set
+            {
+                if (Equals(value, _choices)) return;
+                _choices = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SelectedValue
+        {
+            get { return _selectedValue; }
+            set
+            {
+                if (value == _selectedValue) return;
+                _selectedValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Details
+        {
+            get { return _details; }
+            set
+            {
+                if (value == _details) return;
+                _details = value;
+                OnPropertyChanged();
+            }
+        }
 
         public ChoiceConfig(object parent, MemberInfo member)
             : base(parent, member)
@@ -163,11 +350,12 @@ namespace WpfSettings.Config
     {
         private static int _id = 0;
         public string GroupName { get; }
-        public ICommand OnSelectionCommand => new RelayCommand<string>(ChangeSelection);
+        public ICommand OnSelectionCommand { get; }
 
         public RadioButtonsConfig(object parent, MemberInfo member)
             : base(parent, member)
         {
+            OnSelectionCommand = new RelayCommand<string>(ChangeSelection);
             GroupName = Member.Name + _id++;
         }
 
